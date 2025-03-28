1bebdfae9d0017a53eea61c4237e98745b266a8d
Lika Sobolenko <lika160892@yandex.ru>
2025-03-28 19:15:27 +0700

    Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { NewsComponent } from './news/news.component';
+import { EventsComponent } from './events/events.component';
+import { AcknowledgmentComponent } from './acknowledgment/acknowledgment.component';
+import { DocumentsComponent } from './documents/documents.component';
+import { FoundersComponent } from './founders/founders.component';
+import { HelpsComponent } from './helps/helps.component';
+import { ProgrammsComponent } from './programms/programms.component';
+import { PartnersComponent } from './partners/partners.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'main', component: MainComponent },
+    { path: 'news', component: NewsComponent },
+    { path: 'events', component: EventsComponent },
+    { path: 'acknowledgment', component: AcknowledgmentComponent },
+    { path: 'documents', component: DocumentsComponent },
+    { path: 'founders', component: FoundersComponent },
+    { path: 'help', component: HelpsComponent },
+    { path: 'programms', component: ProgrammsComponent },
+    { path: 'partners', component: PartnersComponent },
+    { path: '**', component: MainComponent }
+    ];
